66ccd6b04367b6815183603b40d9d67d2392dfe7
JoaoVarela1 <84931110+JoaoVarela1@users.noreply.github.com>
2021-06-10 15:47:31 -0300

    Add files via upload

---
+++ Aula.py
@@ -0,0 +1,27 @@
+ 
+País=["Brasil","Holanda","Espanha","França"]
+print(País)
+
+del País[1]
+print(País)
+
+País.append("Argentina")
+print(País)
+
+País.remove("Brasil")
+print(País)
+
+País.sort()
+print(País)
+
+País=["Brasil","Holanda","Espanha","França"]
+País2=sorted(País)
+print(País)
+print(País2)
+
+País=["Brasil","Holanda","Espanha","França"]
+País.reverse()
+print(País)
+
+País=["Brasil","Holanda","Espanha","França","Luxembourg","Belgica"]
+print(len(País))
